57c9128a3ddc5cc9be16588beb74967ac566c7cc
Isaack Mwenda <mwendaisaac@gmail.com>
2019-09-13 16:18:21 +0300

    add follow up questions configurations

--- src/lib/code_schemes.py
+++ src/lib/code_schemes.py
@@ -25,4 +25,7 @@ class CodeSchemes(object):
     RECENTLY_DISPLACED = _open_scheme("recently_displaced.json")
     IN_IDP_CAMP = _open_scheme("in_idp_camp.json")
 
+    HAVE_VOICE_YES_NO_AMB = _open_scheme("have_voice_yes_no_amb.json")
+    SUGGESTIONS = _open_scheme("suggestions.json")
+
     WS_CORRECT_DATASET = _open_scheme("ws_correct_dataset.json")
